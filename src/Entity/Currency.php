1b57a252862ba3236e3529b61e073e287ac4fb45
Stephan Altmann <stephan.altmann@AGAN-MAC020.fritz.box>
2024-01-01 16:56:58 +0100

    install api platform

--- src/Entity/Currency.php
+++ src/Entity/Currency.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CurrencyRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -11,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CurrencyRepository::class)]
 #[ORM\HasLifecycleCallbacks]
+#[ApiResource]
 class Currency
 {
     // TODO: on update (only if value has changed -> preUpdate)
